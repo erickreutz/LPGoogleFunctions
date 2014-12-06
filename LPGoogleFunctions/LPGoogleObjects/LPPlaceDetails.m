8e7f716ecfd5d3e0700a8d6adb12d4c1e7050b6f
Eric Kreutzer <eric@airkrft.com>
2014-12-06 15:02:48 -0800

    set addressComponents in LPPlaceDetails#copyWithZone

--- LPGoogleFunctions/LPGoogleObjects/LPPlaceDetails.m
+++ LPGoogleFunctions/LPGoogleObjects/LPPlaceDetails.m
@@ -30,7 +30,7 @@
         self.rating = [coder decodeFloatForKey:@"rating"];
         self.events = [coder decodeObjectForKey:@"events"];
 	}
-	
+
 	return self;
 }
 
@@ -56,99 +56,99 @@
 + (id)placeDetailsWithObjects:(NSDictionary *)dictionary
 {
     LPPlaceDetails *new = [LPPlaceDetails new];
-    
+
     if (![dictionary isKindOfClass:[NSNull class]]) {
         if (![[dictionary objectForKey:@"address_components"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"address_components"]) {
             NSMutableArray *array = [NSMutableArray new];
-            
+
             for (int i=0; i<[[dictionary objectForKey:@"address_components"] count]; i++) {
                 LPAddressComponent *addressComponent = [LPAddressComponent addressComponentWithObjects:[[dictionary objectForKey:@"address_components"] objectAtIndex:i]];
-                
+
                 [array addObject:addressComponent];
             }
-            
+
             new.addressComponents = [NSArray arrayWithArray:array];
         }
-        
+
         if (![[dictionary objectForKey:@"adr_address"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"adr_address"]) {
             new.adrAddress = [dictionary objectForKey:@"adr_address"];
         }
-        
+
         if (![[dictionary objectForKey:@"formatted_address"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"formatted_address"]) {
             new.formattedAddress = [dictionary objectForKey:@"formatted_address"];
         }
-        
+
         if (![[dictionary objectForKey:@"geometry"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"geometry"]) {
             new.geometry = [LPGeometry geometryWithObjects:[dictionary objectForKey:@"geometry"]];
         }
-        
+
         if (![[dictionary objectForKey:@"icon"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"icon"]) {
             new.icon = [dictionary objectForKey:@"icon"];
         }
-        
+
         if (![[dictionary objectForKey:@"id"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"id"]) {
             new.ID=[dictionary objectForKey:@"id"];
         }
-        
+
         if (![[dictionary objectForKey:@"name"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"name"]) {
             new.name=[dictionary objectForKey:@"name"];
         }
-        
+
         if (![[dictionary objectForKey:@"reference"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"reference"]) {
             new.reference=[dictionary objectForKey:@"reference"];
         }
-        
+
         if (![[dictionary objectForKey:@"types"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"types"]) {
             NSMutableArray *array = [NSMutableArray new];
-            
+
             for (int i=0; i<[[dictionary objectForKey:@"types"] count]; i++) {
                 [array addObject:[[dictionary objectForKey:@"types"] objectAtIndex:i]];
             }
-            
+
             new.types = [NSArray arrayWithArray:array];
         }
-        
+
         if (![[dictionary objectForKey:@"url"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"url"]) {
             new.url = [dictionary objectForKey:@"url"];
         }
-        
+
         if (![[dictionary objectForKey:@"vicinity"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"vicinity"]) {
             new.vicinity=[dictionary objectForKey:@"vicinity"];
         }
-        
+
         if (![[dictionary objectForKey:@"photos"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"photos"]) {
             NSMutableArray *array = [NSMutableArray new];
-            
+
             for (int i=0; i<[[dictionary objectForKey:@"photos"] count]; i++) {
                 LPPlacePhoto *photo = [LPPlacePhoto placePhotoWithObjects:[[dictionary objectForKey:@"photos"] objectAtIndex:i]];
-                                       
+
                 [array addObject:photo];
             }
-            
+
             new.photos = [NSArray arrayWithArray:array];
         }
-        
+
         if (![[dictionary objectForKey:@"price_level"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"price_level"]) {
             new.priceLevel = [[dictionary objectForKey:@"price_level"] intValue];
         }
-        
+
         if (![[dictionary objectForKey:@"rating"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"rating"]) {
             new.rating = [[dictionary objectForKey:@"rating"] floatValue];
         }
-        
+
         if (![[dictionary objectForKey:@"events"] isKindOfClass:[NSNull class]] && [dictionary objectForKey:@"events"]) {
             NSMutableArray *array = [NSMutableArray new];
-            
+
             for (int i=0; i<[[dictionary objectForKey:@"events"] count]; i++) {
                 LPEvent *event = [LPEvent eventWithObjects:[[dictionary objectForKey:@"events"] objectAtIndex:i]];
-                
+
                 [array addObject:event];
             }
-            
+
             new.events = [NSArray arrayWithArray:array];
         }
     }
-    
+
     return new;
 }
 
@@ -159,54 +159,54 @@
     if (self.geometry && ![self.geometry isKindOfClass:[NSNull class]]) {
         [dictionary setObject:self.geometry.dictionary forKey:@"geometry"];
     }
-    
+
     if (self.addressComponents && ![self.addressComponents isKindOfClass:[NSNull class]]) {
         NSMutableArray *array = [NSMutableArray new];
-        
+
         for (int i=0; i<[self.addressComponents count]; i++) {
             [array addObject:((LPAddressComponent *)[self.addressComponents objectAtIndex:i]).dictionary];
         }
-        
+
         [dictionary setObject:array forKey:@"addressComponents"];
     }
-    
+
     if (self.types && ![self.types isKindOfClass:[NSNull class]]) {
         NSMutableArray *array = [NSMutableArray new];
-        
+
         for (int i=0; i<[self.types count]; i++) {
             [array addObject:[self.types objectAtIndex:i]];
         }
-        
+
         [dictionary setObject:array forKey:@"types"];
     }
-    
+
     if (self.photos && ![self.photos isKindOfClass:[NSNull class]]) {
         NSMutableArray *array = [NSMutableArray new];
-        
+
         for (int i=0; i<[self.photos count]; i++) {
             LPPlacePhoto *photo = (LPPlacePhoto *)[self.photos objectAtIndex:i];
-            
+
             [array addObject:photo.dictionary];
         }
-        
+
         [dictionary setObject:array forKey:@"photos"];
     }
-    
+
     [dictionary setObject:[NSString stringWithFormat:@"%d", self.priceLevel] forKey:@"priceLevel"];
     [dictionary setObject:[NSString stringWithFormat:@"%.1f", self.rating] forKey:@"rating"];
-    
+
     if (self.events && ![self.events isKindOfClass:[NSNull class]]) {
         NSMutableArray *array = [NSMutableArray new];
-        
+
         for (int i=0; i<[self.events count]; i++) {
             LPEvent *event = (LPEvent *)[self.events objectAtIndex:i];
-            
+
             [array addObject:event.dictionary];
         }
-        
+
         [dictionary setObject:array forKey:@"events"];
     }
-    
+
     return dictionary;
 }
 
@@ -218,8 +218,9 @@
 - (id)copyWithZone:(NSZone *)zone
 {
     LPPlaceDetails *new = [LPPlaceDetails new];
-    
+
     [new setAdrAddress:[self adrAddress]];
+		[new setAddressComponents:[self addressComponents]];
     [new setFormattedAddress:[self formattedAddress]];
     [new setGeometry:[self geometry]];
     [new setIcon:[self icon]];
@@ -231,7 +232,7 @@
     [new setVicinity:[self vicinity]];
     [new setPriceLevel:self.priceLevel];
     [new setEvents:self.events];
-    
+
     return new;
 }
 
